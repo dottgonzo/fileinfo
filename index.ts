cf329f383d79c5ff49d9e453b8d5b58c961ad3dc
Dario Caruso <dev@dariocaruso.info>
2017-07-26 03:58:06 +0200

    add file kind extension based

--- index.ts
+++ index.ts
@@ -5,6 +5,9 @@ interface Iinfo {
     name: string
     extension: string
     dir: string
+    extensionFamily: string
+    extensionType: string
+
 }
 
 export function filenameinfo(file: string): Iinfo {
@@ -14,6 +17,70 @@ export function filenameinfo(file: string): Iinfo {
     info.dir = file.split('/' + info.fullname)[0]
     info.extension = info.fullname.split('.')[info.fullname.split('.').length - 1]
     info.name = info.fullname.split('.' + info.extension)[info.fullname.split('.' + info.extension).length - 2]
+
+
+    switch (info.extension) {
+
+        case "mp4":
+            info.extensionFamily = 'video'
+            info.extensionType = 'mpeg4'
+
+            break
+        case "flv":
+            info.extensionFamily = 'video'
+            info.extensionType = 'mpeg4'
+
+            break
+        case "avi":
+            info.extensionFamily = 'video'
+            info.extensionType = 'unknown'
+
+            break
+        case "webm":
+            info.extensionFamily = 'video'
+            info.extensionType = 'webmin'
+
+            break
+        case "mkv":
+            info.extensionFamily = 'video'
+            info.extensionType = 'matrioska'
+
+            break
+        case "mp3":
+            info.extensionFamily = 'audio'
+            info.extensionType = 'mpeg3'
+
+            break
+        case "mpa":
+            info.extensionFamily = 'audio'
+            info.extensionType = 'mpeg4audio'
+
+            break
+        case "aac":
+            info.extensionFamily = 'audio'
+            info.extensionType = 'mpeg4audio'
+
+            break
+        case "ogg":
+            info.extensionFamily = 'audio'
+            info.extensionType = 'unknown'
+
+            break
+        case "flac":
+            info.extensionFamily = 'audio'
+            info.extensionType = 'unknown'
+
+            break
+        default:
+            info.extensionFamily = 'unknown'
+
+            info.extensionType = 'unknown'
+            break
+    }
+
+
+
+
     return info
 
 }
--- test/main.ts
+++ test/main.ts
@@ -3,26 +3,26 @@ import * as fileinfo from '../index'
 import * as chai from 'chai'
 const expect = chai.expect
 
-console.log(fileinfo.filenameinfo('/boot/grub/grub.cfg'))
+console.log(fileinfo.filenameinfo(__dirname+'/ketto.mp4'))
 describe('main test fileinfo', function () {
     it('it works', function (done) {
-        expect(fileinfo.filenameinfo('/boot/grub/grub.cfg')).to.be.ok
+        expect(fileinfo.filenameinfo(__dirname+'/ketto.mp4')).to.be.ok
         done()
     })
     it('contains the path as string', function (done) {
-        expect(fileinfo.filenameinfo('/boot/grub/grub.cfg')).have.property('path').that.is.a('string')
+        expect(fileinfo.filenameinfo(__dirname+'/ketto.mp4')).have.property('path').that.is.a('string')
         done()
     })
     it('contains the extension as string', function (done) {
-        expect(fileinfo.filenameinfo('/boot/grub/grub.cfg')).have.property('extension').that.is.a('string')
+        expect(fileinfo.filenameinfo(__dirname+'/ketto.mp4')).have.property('extension').that.is.a('string')
         done()
     })
     it('contains the name as string', function (done) {
-        expect(fileinfo.filenameinfo('/boot/grub/grub.cfg')).have.property('name').that.is.a('string')
+        expect(fileinfo.filenameinfo(__dirname+'/ketto.mp4')).have.property('name').that.is.a('string')
         done()
     })
     it('contains the fullname as string', function (done) {
-        expect(fileinfo.filenameinfo('/boot/grub/grub.cfg')).have.property('fullname').that.is.a('string')
+        expect(fileinfo.filenameinfo(__dirname+'/ketto.mp4')).have.property('fullname').that.is.a('string')
         done()
     })
 })
